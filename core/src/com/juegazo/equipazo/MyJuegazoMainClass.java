4caa29e04829af85ed7b8c55af46b091447a998a
Julio Jesús Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2016-12-21 17:22:41 +0100

    Change arrow size

--- core/src/com/juegazo/equipazo/MyJuegazoMainClass.java
+++ core/src/com/juegazo/equipazo/MyJuegazoMainClass.java
@@ -4,11 +4,13 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyJuegazoMainClass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+    private int size = 50;
 	
 	@Override
 	public void create () {
@@ -21,7 +23,7 @@ public class MyJuegazoMainClass extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+        batch.draw(img, 0, 0, size, size);
 		batch.end();
 	}
 	

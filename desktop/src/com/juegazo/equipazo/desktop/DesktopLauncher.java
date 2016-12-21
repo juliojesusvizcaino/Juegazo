2f4800b60fae7989b35c3b3a0d6ea1ea99d91657
Julio Jesús Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2016-12-21 16:45:13 +0100

    Add initial commit

---
+++ desktop/src/com/juegazo/equipazo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.juegazo.equipazo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.juegazo.equipazo.MyJuegazoMainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyJuegazoMainClass(), config);
+	}
+}
